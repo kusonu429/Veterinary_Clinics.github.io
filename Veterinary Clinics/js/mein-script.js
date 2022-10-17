39b038dbb37ce786a460dc979ffbe2c61f4d8340
Sonu <98530388+kusonu429@users.noreply.github.com>
2022-10-17 18:33:03 +0530

    Update CSS
    Update HTML CSS

---
+++ Veterinary Clinics/js/mein-script.js
@@ -0,0 +1,18 @@
+
+
+// Humburger menu 
+$(function () {
+
+    
+  $(".humburger").click(function () {
+
+    $("ul").toggleClass("active");
+
+  });
+   
+});
+
+// Humburger menu 
+              
+
+// alert("Hello! I am an alert box!!");
